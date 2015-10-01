f78744da6de010bc4e3860b60e2b7db6946b4eef
danielsmithu <smi14089@uvg.edu.gt>
2015-10-01 07:51:00 -0600

    Update Paciente.java

--- Paciente.java
+++ Paciente.java
@@ -1,4 +1,31 @@
 import java.util.PriorityQueue;
 public class Paciente<E> extends Comparable<E> implements PriorityQueue {
+  public Paciente(String nombre, String enfermedad, char codigoEmergencia){
+  this.nombre = nombre;
+  this.enfermedad = enfermedad;
+  this.codigoEmergencia = codigoEmergencia;
   
+  public setNombre(String nombre){
+    this.nombre = nombre;
+  }
+  
+  public getNombre(){
+    return nombre;
+  }
+  
+  public setEnfermedad(String enfermedad){
+    this.enfermedad = enfermedad;
+  }
+  
+  public getEnfermedad(){
+    return enfermedad;
+  }
+  
+  public setCodigoEmergencia(char codigoEmergencia){
+    this.codigoEmergencia = codigoEmergencia;
+  }
+  
+  public getCodigoEmergencia(){
+    return codigoEmergencia;
+  }
 }
